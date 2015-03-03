49a087525c2e5593dd00ea1936235679da4b393e
Joel Trew <joel@trew.org.uk>
2015-03-03 19:11:38 +0000

    Created custom cell class

---
+++ BBCNews/JLTNewsStoryTableViewCell.m
@@ -0,0 +1,16 @@
+//
+//  JLTNewsStoryTableViewCell.m
+//  BBCNews
+//
+//  Created by Joel Trew on 03/03/2015.
+//  Copyright (c) 2015 Joel Trew. All rights reserved.
+//
+
+#import "JLTNewsStoryTableViewCell.h"
+
+@implementation JLTNewsStoryTableViewCell
+
+- (instancetype)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+
+
+@end
