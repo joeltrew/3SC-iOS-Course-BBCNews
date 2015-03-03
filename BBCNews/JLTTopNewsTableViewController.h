f33f9fa8a5eef6308e109114736bdf6f82b9079b
Joel Trew <joel@trew.org.uk>
2015-03-03 18:30:21 +0000

    created new table view controllers

---
+++ BBCNews/JLTTopNewsTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  JLTTopNewsTableViewController.h
+//  BBCNews
+//
+//  Created by Joel Trew on 03/03/2015.
+//  Copyright (c) 2015 Joel Trew. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JLTTopNewsTableViewController : UITableViewController
+
+@end
---
+++ BBCNews/JLTTopNewsTableViewController.m
@@ -0,0 +1,56 @@
+//
+//  JLTTopNewsTableViewController.m
+//  BBCNews
+//
+//  Created by Joel Trew on 03/03/2015.
+//  Copyright (c) 2015 Joel Trew. All rights reserved.
+//
+
+#import "JLTTopNewsTableViewController.h"
+
+@interface JLTTopNewsTableViewController ()
+
+@end
+
+@implementation JLTTopNewsTableViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+    
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+#warning Potentially incomplete method implementation.
+    // Return the number of sections.
+    return 0;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+#warning Incomplete method implementation.
+    // Return the number of rows in the section.
+    return 0;
+}
+
+/*
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
+    
+    // Configure the cell...
+    
+    return cell;
+}
+*/
+
+@end
