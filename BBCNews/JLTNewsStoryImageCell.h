2b801b623a5bbe358c142fb61d4746e1836ef86f
Joel Trew <joel@trew.org.uk>
2015-03-10 19:36:54 +0000

    Creates new Image Cell class

---
+++ BBCNews/JLTNewsStoryImageCell.h
@@ -0,0 +1,13 @@
+//
+//  JLTNewsStoryImageCell.h
+//  BBCNews
+//
+//  Created by Joel Trew on 10/03/2015.
+//  Copyright (c) 2015 Joel Trew. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JLTNewsStoryImageCell : UITableViewCell
+
+@end
---
+++ BBCNews/JLTNewsStoryImageCell.m
@@ -0,0 +1,23 @@
+//
+//  JLTNewsStoryImageCell.m
+//  BBCNews
+//
+//  Created by Joel Trew on 10/03/2015.
+//  Copyright (c) 2015 Joel Trew. All rights reserved.
+//
+
+#import "JLTNewsStoryImageCell.h"
+
+@implementation JLTNewsStoryImageCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
