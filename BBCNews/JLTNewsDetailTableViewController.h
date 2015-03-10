84ed2786a34579444d3da2020168456bed43e7aa
Joel Trew <joel@trew.org.uk>
2015-03-10 18:28:32 +0000

    Creates new News Detail View Controller

---
+++ BBCNews/JLTNewsDetailTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  JLTNewsDetailTableViewController.h
+//  BBCNews
+//
+//  Created by Joel Trew on 10/03/2015.
+//  Copyright (c) 2015 Joel Trew. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JLTNewsDetailTableViewController : UITableViewController
+
+@end
