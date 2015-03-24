52bd56c21de9695030d1d572b3efdcc9489daa22
Joel Trew <joel@trew.org.uk>
2015-03-24 18:40:49 +0000

    Creates new news controller

---
+++ BBCNews/JLTNewsController.h
@@ -0,0 +1,13 @@
+//
+//  JLTNewsController.h
+//  BBCNews
+//
+//  Created by Joel Trew on 24/03/2015.
+//  Copyright (c) 2015 Joel Trew. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JLTNewsController : NSObject
+
+@end
---
+++ BBCNews/JLTNewsController.m
@@ -0,0 +1,13 @@
+//
+//  JLTNewsController.m
+//  BBCNews
+//
+//  Created by Joel Trew on 24/03/2015.
+//  Copyright (c) 2015 Joel Trew. All rights reserved.
+//
+
+#import "JLTNewsController.h"
+
+@implementation JLTNewsController
+
+@end
